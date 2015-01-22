d7ca00a66193780fc810c7a34ad21bff49801cff
resteve <resteve@zikzakmedia.com>
2015-01-22 13:09:39 +0100

    Header module name

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
-#This file is part sale_jreport module for Tryton.
+#This file is part account_invoice_jreport module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
 from .account_invoice import *
-
 
 def register():
     Pool.register(
