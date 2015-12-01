a201ab93cd4c186677a72f505e9c0cded9b4d76a
resteve <resteve@zikzakmedia.com>
2015-12-01 10:57:14 +0100

    Tests

--- tests/test_account_invoice_jreport.py
+++ tests/test_account_invoice_jreport.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-#This file is part account_invoice_jreport module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part of the account_invoice_jreport module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoiceJreportTestCase(unittest.TestCase):
-    '''
-    Test Account Invoice Jasper Report module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_jreport')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+class AccountInvoiceJreportTestCase(ModuleTestCase):
+    'Test Account Invoice Jreport module'
+    module = 'account_invoice_jreport'
 
 
 def suite():
@@ -27,4 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountInvoiceJreportTestCase))
     return suite
-
