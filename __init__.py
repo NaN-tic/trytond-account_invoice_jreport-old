71c989f2cc08cff8fedba92585444067e610dfa1
Raimon Esteve <raimon@nan-tic.com>
2017-02-07 07:35:53 +0100

    Register classes

--- __init__.py
+++ __init__.py
@@ -2,12 +2,12 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
-from .account_invoice import *
+from . import invoice
 
 def register():
     Pool.register(
-        Invoice,
+        invoice.Invoice,
         module='account_invoice_jreport', type_='model')
     Pool.register(
-        InvoiceReport,
+        invoice.InvoiceReport,
         module='account_invoice_jreport', type_='report')
